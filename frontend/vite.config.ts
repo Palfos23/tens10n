90fe3260a48b08a38e4080b4541fab12d1979d18
Palfos23 <paul.fossnes@gmail.com>
2025-10-27 14:22:26 +0100

    Deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: 'https://palfos23.github.io/tens10n/',   // <- bruk ditt repo-navn her
+    plugins: [react()]
 })
